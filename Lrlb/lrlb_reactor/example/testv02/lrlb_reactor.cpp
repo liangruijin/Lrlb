4e7b2678ee87e28df364c593d372679ef9c0074d
liangruijin <377963386@qq.com>
2021-01-14 17:55:26 +0800

    del

--- Lrlb/lrlb_reactor/example/testv02/lrlb_reactor.cpp
+++
@@ -1,9 +0,0 @@
-#include "tcp_server.h"
-
-int main() {
-
-    tcp_server server("192.168.237.128", 7777);
-    server.do_accept();
-
-    return 0;
-}
--- Lrlb/lrlb_reactor/example/testv03/lrlb_reactor.cpp
+++
@@ -1,9 +0,0 @@
-#include "tcp_server.h"
-
-int main() {
-	event_loop loop;
-    tcp_server server(&loop,"192.168.237.128", 7777);
-    
-	loop.event_process();
-    return 0;
-}
